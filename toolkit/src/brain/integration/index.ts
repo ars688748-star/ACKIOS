b9adb399830867f929db7e77e0726cab65df0c58
Арсен <your@email.com>
2026-07-10 18:37:10 +0300

    Sprint 58 - Reasoning bridge

--- toolkit/src/brain/integration/index.ts
+++ toolkit/src/brain/integration/index.ts
@@ -1,3 +1,4 @@
 ﻿export * from "./BrainRuntimeBridge.js";
 export * from "./memory/index.js";
 export * from "./knowledge/index.js";
+export * from "./reasoning/index.js";
---
+++ toolkit/src/brain/integration/reasoning/ReasoningBridge.ts
@@ -0,0 +1,7 @@
+﻿export class ReasoningBridge{
+
+    async execute():Promise<void>{
+
+    }
+
+}
