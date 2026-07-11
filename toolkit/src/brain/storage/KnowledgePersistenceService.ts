c8dca052c6f7e40cb7d5bee10d0237a4e238b6d1
Арсен <your@email.com>
2026-07-11 14:14:58 +0300

    feat(storage): improve knowledge persistence service

--- toolkit/src/brain/storage/KnowledgePersistenceService.ts
+++ toolkit/src/brain/storage/KnowledgePersistenceService.ts
@@ -1,40 +1,86 @@
 ﻿import { BrainService } from "../services/BrainService.js";
+
 import { KnowledgeManager } from "../knowledge/KnowledgeManager.js";
+
 import { BrainKnowledgeExporter } from "./BrainKnowledgeExporter.js";
+import { BrainKnowledgeImporter } from "./BrainKnowledgeImporter.js";
+
 import { BrainStorage } from "./BrainStorage.js";
 
 export class KnowledgePersistenceService extends BrainService {
 
     public readonly name = "KnowledgePersistenceService";
 
-    constructor(
+    public constructor(
+
         private readonly knowledge: KnowledgeManager,
+
         private readonly exporter: BrainKnowledgeExporter,
+
+        private readonly importer: BrainKnowledgeImporter,
+
         private readonly storage: BrainStorage
+
     ){
+
         super();
-    }
-
-
-    public save(): void {
-
-        const projectKnowledge = this.exporter.export(this.knowledge);
-
-        this.storage.save(projectKnowledge);
 
     }
 
+    public override async initialize(): Promise<void>{
 
-    public load(): void {
+        this.restore();
 
-        this.storage.load();
+    }
+
+    public restore(): void{
+
+        const project = this.storage.load();
+
+        if(project === null){
+
+            return;
+
+        }
+
+        this.importer.import(
+
+            this.knowledge,
+
+            project
+
+        );
+
+    }
+
+    public save(): void{
+
+        const project = this.exporter.export(
+
+            this.knowledge
+
+        );
+
+        this.storage.save(
+
+            project
+
+        );
 
     }
 
 
-    public clear(): void {
+    public clear(): void{
 
         this.storage.clear();
+
+        this.knowledge.clear();
+
+    }
+
+    public override async dispose(): Promise<void>{
+
+        this.save();
 
     }
 
