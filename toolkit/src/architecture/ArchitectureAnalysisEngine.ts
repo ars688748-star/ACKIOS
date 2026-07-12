6a6e93db7deb5fd9fb9aebb49b8673f4fc006f1c
Арсен <your@email.com>
2026-07-12 19:53:26 +0300

    feat(architecture): add architecture analysis engine

---
+++ toolkit/src/architecture/ArchitectureAnalysisEngine.ts
@@ -0,0 +1,43 @@
+﻿import { ArchitectureRuleEngine } from "./ArchitectureRuleEngine.js";
+import { GraphBuilder } from "./graph/GraphBuilder.js";
+import type { ArchitectureReport } from "./model/ArchitectureReport.js";
+import { ArchitectureParser } from "./parser/ArchitectureParser.js";
+import { ArchitectureScanner } from "./scanner/ArchitectureScanner.js";
+
+export class ArchitectureAnalysisEngine {
+
+    public constructor(
+
+        private readonly scanner =
+            new ArchitectureScanner(),
+
+        private readonly parser =
+            new ArchitectureParser(),
+
+        private readonly graphBuilder =
+            new GraphBuilder(),
+
+        private readonly ruleEngine =
+            new ArchitectureRuleEngine([])
+
+    ) {}
+
+    public async analyze(
+        root: string
+    ): Promise<ArchitectureReport> {
+
+        await this.scanner.scan(root);
+
+        return {
+
+            scannedFiles: 0,
+
+            scannedDirectories: 0,
+
+            violations: []
+
+        };
+
+    }
+
+}
